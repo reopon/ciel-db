b8b0711fbab6d97f42bcfe98041a83ee69d1541a
reopon <reo.ooc@gmail.com>
2025-06-27 16:42:23 +0900

    linst消し

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Geist, Geist_Mono } from "next/font/google";
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import { Menu, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
